35f8e53dedc9ac8e44ef0738809ff612bdc477c3
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-03-21 11:52:17 -0300

    comit

--- leafletjs.js
+++ leafletjs.js
@@ -4,3 +4,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var circle = L.circle([-23.2171359, -45.8754657], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
