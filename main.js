305964aff3453ce670d5ca911407810c47f5cf84
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-03-20 22:23:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const findMyState = () => {
+    const status = document.querySelector('.status');
+
+    const success = (position) =>{
+        console.log(position)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        //console.log(latitude + ' ' + longitude)
+
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+        fetch(geoApiUrl)
+        .then(res => res.json()
+        .then(data => {
+            console.log(data)
+           //status.textContent = data.city
+           status.textContent = data.locality
+           //status.textContent = data.city
+           
+        }))
+    }
+
+    const error = () => {
+        status.textContent = 'Localização não encontrada.';
+    }
+
+    navigator.geolocation.getCurrentPosition(success,  error);
+}
+
+document.querySelector('.find-state').addEventListener('click', findMyState);
+
